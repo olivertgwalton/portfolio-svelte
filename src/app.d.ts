c0683b4ea98689d5cc7538335c01601429e0591b
Oliver Walton <oliverharrison03@gmail.com>
2026-02-04 13:04:57 +0000

    fix(types): add theme definition to App.Locals interface
    - Resolves TS2339 error for locals.theme property.

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,7 +3,9 @@
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			theme: 'light' | 'dark' | undefined;
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
