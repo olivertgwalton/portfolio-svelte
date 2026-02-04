ce6d61181a70ade752b499eae4a716932220a39b
Oliver Walton <oliverharrison03@gmail.com>
2026-02-04 01:44:33 +0000

    style: update global layout with fonts, colors, and admin structure

---
+++ src/routes/(protected)/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import '@fontsource-variable/space-grotesk';
+	import '@fontsource-variable/manrope';
+	import '@fontsource-variable/jetbrains-mono';
+	import { Toaster } from 'svelte-sonner';
+	import Navbar from '$lib/components/Navbar.svelte';
+
+	let { children } = $props();
+</script>
+
+<Navbar />
+<Toaster position="bottom-right" richColors />
+
+{@render children()}
