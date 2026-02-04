573ecf75e6a721460d3732476de0d539b8afc5c2
Oliver Walton <oliverharrison03@gmail.com>
2026-02-04 19:39:43 +0000

    fix(vite): allow deployed url

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,9 @@ import { enhancedImages } from '@sveltejs/enhanced-img';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({ plugins: [tailwindcss(), enhancedImages(), sveltekit()] });
+export default defineConfig({
+	plugins: [tailwindcss(), enhancedImages(), sveltekit()],
+	preview: {
+		allowedHosts: ['oliverwalton.uk']
+	}
+});
