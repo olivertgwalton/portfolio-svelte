4877e79c77c27582dfeb4230f663c9e16d568f71
olivertgwalton <oliverharrison03@gmail.com>
2026-02-13 19:41:24 +0000

    chore: move import to bun adapter

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,29 +1,25 @@
 <script lang="ts">
-	import './layout.css';
-	import '@fontsource-variable/space-grotesk';
-	import '@fontsource-variable/manrope';
-	import '@fontsource-variable/jetbrains-mono';
-	import { injectAnalytics } from '@vercel/analytics/sveltekit';
-	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
-	import MetaTags from '$lib/components/MetaTags.svelte';
-	import Navbar from '$lib/components/Navbar.svelte';
-	import BackToTop from '$lib/components/BackToTop.svelte';
+    import "./layout.css";
+    import "@fontsource-variable/space-grotesk";
+    import "@fontsource-variable/manrope";
+    import "@fontsource-variable/jetbrains-mono";
+    import MetaTags from "$lib/components/MetaTags.svelte";
+    import Navbar from "$lib/components/Navbar.svelte";
+    import BackToTop from "$lib/components/BackToTop.svelte";
 
-	injectAnalytics();
-	injectSpeedInsights();
-	let { children, data } = $props();
+    let { children, data } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href="/favicon.svg">
+    <link rel="icon" href="/favicon.svg" />
 </svelte:head>
 
 <MetaTags />
 
 <div class="flex min-h-screen flex-col">
-	<Navbar theme={data.theme} mode={data.mode} />
-	<main class="grow">
-		{@render children()}
-	</main>
-	<BackToTop />
+    <Navbar theme={data.theme} mode={data.mode} />
+    <main class="grow">
+        {@render children()}
+    </main>
+    <BackToTop />
 </div>
