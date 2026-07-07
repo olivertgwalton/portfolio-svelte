b7ee1720f7ae97c3c68a05f1a90f3b73227235e3
oliverwalton <oliverharrison03@gmail.com>
2026-07-07 10:50:25 +0100

    chore(eslint): make use of typescript

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,11 +1,9 @@
 import path from 'node:path';
-import { includeIgnoreFile } from '@eslint/compat';
-import js from '@eslint/js';
+import { includeIgnoreFile } from '@eslint/config-helpers';
 import svelte from 'eslint-plugin-svelte';
 import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import ts from 'typescript-eslint';
-import svelteConfig from './svelte.config.js';
 
 const gitignorePath = path.resolve(import.meta.dirname, '.gitignore');
 
@@ -16,12 +14,17 @@ export default defineConfig(
 		ignores: ['src/lib/wasm/', 'static/wasm/', 'rust-grid/']
 	},
 
-	js.configs.recommended,
-
-	...ts.configs.recommended,
-	...svelte.configs.recommended,
 	{
-		languageOptions: { globals: { ...globals.browser, ...globals.node } },
+		extends: [ts.configs.recommendedTypeChecked, svelte.configs.recommended],
+		languageOptions: {
+			globals: { ...globals.browser, ...globals.node },
+			parserOptions: {
+				tsconfigRootDir: import.meta.dirname,
+				projectService: {
+					allowDefaultProject: ['eslint.config.ts', 'svelte.config.ts', 'src/lib/workers/*.js']
+				}
+			}
+		},
 		rules: {
 			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
 			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
@@ -32,10 +35,8 @@ export default defineConfig(
 		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
 		languageOptions: {
 			parserOptions: {
-				projectService: true,
 				extraFileExtensions: ['.svelte'],
-				parser: ts.parser,
-				svelteConfig
+				parser: ts.parser
 			}
 		}
 	}
